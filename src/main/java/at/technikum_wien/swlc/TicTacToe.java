d2dfe15e9c3d7782ca15ddf23a8a67b55651fc05
Dhwani Monish Gogri <wi25b140@technikum-wien.at>
2026-05-26 18:41:41 +0200

    refactor: split start method in TicTacToe to comply with checkstyle MethodLength limits

--- src/main/java/at/technikum_wien/swlc/TicTacToe.java
+++ src/main/java/at/technikum_wien/swlc/TicTacToe.java
@@ -21,62 +21,7 @@ public class TicTacToe {
         boolean playAgain = true;
 
         while (playAgain) {
-            board.clear();
-            currentPlayer = player1;
-            boolean gameEnded = false;
-
-            while (!gameEnded) {
-                System.out.println("Current Player: " + currentPlayer.getMarker());
-                board.print();
-
-                int row = -1;
-                int column = -1;
-                boolean validMove = false;
-
-                while (!validMove) {
-                    System.out.print("row (0-2): ");
-                    if (sc.hasNextInt()) {
-                        row = sc.nextInt();
-                    } else {
-                        System.out.println("Invalid input. Please enter a number between 0 and 2.");
-                        sc.next();
-                        continue;
-                    }
-
-                    System.out.print("column (0-2): ");
-                    if (sc.hasNextInt()) {
-                        column = sc.nextInt();
-                    } else {
-                        System.out.println("Invalid input. Please enter a number between 0 and 2.");
-                        sc.next();
-                        continue;
-                    }
-
-                    if (row >= 0 && row < 3 && column >= 0 && column < 3) {
-                        if (board.isCellEmpty(row, column)) {
-                            validMove = true;
-                        } else {
-                            System.out.println("Choose another cell, already occupied!");
-                        }
-                    } else {
-                        System.out.println("Invalid coordinates. Row and column must be between 0 and 2.");
-                    }
-                }
-
-                board.place(row, column, currentPlayer.getMarker());
-
-                if (hasWinner()) {
-                    board.print();
-                    System.out.println("Player " + currentPlayer.getMarker() + " wins!");
-                    gameEnded = true;
-                } else if (board.isFull()) {
-                    board.print();
-                    System.out.println("The game is a draw!");
-                    gameEnded = true;
-                } else {
-                    switchCurrentPlayer();
-                }
-            }
+            playSingleGame(sc);
 
             System.out.print("Do you want to play again? (y/n): ");
             String choice = sc.next().trim().toLowerCase();
@@ -87,6 +32,61 @@ public class TicTacToe {
 
         System.out.println("Thanks for playing!");
         sc.close();
+    }
+
+    private void playSingleGame(Scanner sc) {
+        board.clear();
+        currentPlayer = player1;
+        boolean gameEnded = false;
+
+        while (!gameEnded) {
+            System.out.println("Current Player: " + currentPlayer.getMarker());
+            board.print();
+
+            int[] move = getValidMove(sc);
+            board.place(move[0], move[1], currentPlayer.getMarker());
+
+            if (hasWinner()) {
+                board.print();
+                System.out.println("Player " + currentPlayer.getMarker() + " wins!");
+                gameEnded = true;
+            } else if (board.isFull()) {
+                board.print();
+                System.out.println("The game is a draw!");
+                gameEnded = true;
+            } else {
+                switchCurrentPlayer();
+            }
+        }
+    }
+
+    private int[] getValidMove(Scanner sc) {
+        while (true) {
+            System.out.print("row (0-2): ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number between 0 and 2.");
+                sc.next();
+                continue;
+            }
+            int row = sc.nextInt();
+
+            System.out.print("column (0-2): ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number between 0 and 2.");
+                sc.next();
+                continue;
+            }
+            int col = sc.nextInt();
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3) {
+                if (board.isCellEmpty(row, col)) {
+                    return new int[]{row, col};
+                }
+                System.out.println("Choose another cell, already occupied!");
+            } else {
+                System.out.println("Invalid coordinates. Row and column must be between 0 and 2.");
+            }
+        }
     }
 
     private void switchCurrentPlayer() {
@@ -100,11 +100,19 @@ public class TicTacToe {
     private boolean hasWinner() {
         char[][] c = board.getCells();
         for (int i = 0; i < 3; ++i) {
-            if (c[i][0] != ' ' && c[i][0] == c[i][1] && c[i][1] == c[i][2]) return true;
-            if (c[0][i] != ' ' && c[0][i] == c[1][i] && c[1][i] == c[2][i]) return true;
+            if (c[i][0] != ' ' && c[i][0] == c[i][1] && c[i][1] == c[i][2]) {
+                return true;
+            }
+            if (c[0][i] != ' ' && c[0][i] == c[1][i] && c[1][i] == c[2][i]) {
+                return true;
+            }
         }
-        if (c[0][0] != ' ' && c[0][0] == c[1][1] && c[1][1] == c[2][2]) return true;
-        if (c[0][2] != ' ' && c[0][2] == c[1][1] && c[1][1] == c[2][0]) return true;
+        if (c[0][0] != ' ' && c[0][0] == c[1][1] && c[1][1] == c[2][2]) {
+            return true;
+        }
+        if (c[0][2] != ' ' && c[0][2] == c[1][1] && c[1][1] == c[2][0]) {
+            return true;
+        }
         return false;
     }
 }
